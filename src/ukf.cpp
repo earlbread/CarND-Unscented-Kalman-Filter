a937d3152720fabe7a6c86c2530c4efab09ca2db
Seunghun Lee <waydi1@gmail.com>
2017-05-15 14:36:02 +0900

    Check for divides by zero

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -1,6 +1,7 @@
 #include "ukf.h"
 #include "tools.h"
 #include "Eigen/Dense"
+#include <cfloat>
 #include <iostream>
 
 using namespace std;
@@ -416,7 +417,7 @@ void UKF::UpdateRadar(MeasurementPackage meas_package) {
     // measurement model
     Zsig(0,i) = sqrt(p_x*p_x + p_y*p_y);                        //r
     Zsig(1,i) = atan2(p_y,p_x);                                 //phi
-    Zsig(2,i) = (p_x*v1 + p_y*v2 ) / sqrt(p_x*p_x + p_y*p_y);   //r_dot
+    Zsig(2,i) = (p_x*v1 + p_y*v2 ) / max(DBL_EPSILON, Zsig(0, i));   //r_dot
   }
 
   //add measurement noise covariance matrix
